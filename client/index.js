297117af27b22b2d3fa3ca5967f92c64bfedf166
Anjum Faraz <anjumfaraz10@gmail.com>
2018-12-21 17:46:13 -0600

    feat: Linting

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import numeral from 'numeral';
+/* eslint-disable no-console */
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I paid ${courseValue} for this course!`);
--- server/start-message.js
+++ server/start-message.js
@@ -1,3 +1,5 @@
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
+
 console.log(chalk.green('Server listening on localhost:3000'));
